7795a60f50d13ef0ead7903ea839f9295e156f9c
khadija <infokhad@gmail.com>
2021-05-04 07:17:32 -0700

    3ieme commit

--- src/be/bxlFormation/Duree.java
+++ src/be/bxlFormation/Duree.java
@@ -1,9 +1,47 @@
 package be.bxlFormation;
 
 public class Duree {
-
     private int minute;
     private int seconde;
     private int heure;
     private int jour;
+
+    public Duree (int nbSeconde){
+        this.seconde=nbSeconde;
+    }
+    public Duree(int minute, int seconde, int heure, int jour) {
+
+        this.minute = minute;
+        this.seconde = seconde;
+        this.heure = heure;
+        this.jour = jour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public int getSeconde() {
+        return seconde;
+    }
+
+    public int getHeure() {
+        return heure;
+    }
+
+    public int getJour() {
+        return jour;
+    }
+
+    public String getDuree(){
+        return jour+":"+heure + ":"+minute+" :"+seconde;
+    }
+
+    public void SubDuree(Duree aSoustraire){
+
+    }
+
+    public void addDuree(Duree aAjouter){
+
+    }
 }
